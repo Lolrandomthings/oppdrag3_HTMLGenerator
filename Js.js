9eebe0dd5b60db43174a5a86b7369135b3a502c8
Julia Mathilde Frenning <156354804+Lolrandomthings@users.noreply.github.com>
2025-03-19 12:54:50 +0100

    Css and some code

--- Js.js
+++ Js.js
@@ -1,40 +1,34 @@
 let bodyEl = document.getElementById("body");
 let colorEl = document.getElementById("color");
-
+let textEl = document.getElementById("text");
+let inputEl = document.getElementById("textinput");
 
 bodyEl.addEventListener("change", changeColor);
+inputEl.addEventListener("input", changeText);
 
 function changeColor(e){ 
     console.log(colorEl.value);
-    let change = colorEl.value;
-    console.log(change);
+    let colorChange = colorEl.value;
     
-    if (CSS.supports('color', change)){
+    if (CSS.supports('color', colorChange)){
         colorEl.style.color = "white";
-        bodyEl.style.backgroundColor = change;
+        bodyEl.style.backgroundColor = colorChange;
     }
     else{
-        colorEl.style.color = "Red";
+        colorEl.style.color = "White";
     }
 
+}   
+
+function changeText() {
+    let textChange = inputEl.value;
+    textEl.innerText = textChange;
 }
 
+// Refrance
+// const para = document.createElement("p");
+// const node = document.createTextNode("This is new.");
+// para.appendChild(node);
 
-
-// let buttonEl = document.getElementById("button")
-// let inputEl = document.getElementById("input")
-
-// buttonEl.addEventListener("click", byttFarge)
-// function byttFarge(e){
-//     console.log(inputEl.value)
-//     let Hei = inputEl.value
-//     if (CSS.supports('color', Hei)){
-//         inputEl.style.color = "White"
-//         buttonEl.style.backgroundColor = Hei
-//     }
-//     else{
-//         inputEl.style.color = "Red"
-//         inputEl.value = "* Skriv inn en gyldi verdi *"
-//         inputEl.addEventListener("click", removetext)
-//     }   
-// }
+// const element = document.getElementById("div1");
+// element.appendChild(para);
