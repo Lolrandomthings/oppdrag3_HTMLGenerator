c47ce024fa56dbb72877d1c044eb1d8da56a245e
Julia Mathilde Frenning <156354804+Lolrandomthings@users.noreply.github.com>
2025-03-24 10:56:51 +0100

    Javascript making it so you can change font type

--- Js.js
+++ Js.js
@@ -1,23 +1,25 @@
-let bodyEl = document.getElementById("body");
-let colorEl = document.getElementById("color");
-let textEl = document.getElementById("text");
-let inputEl = document.getElementById("textinput");
+let bodyEl = document.getElementById("body")
+let colorEl = document.getElementById("color")
+let textEl = document.getElementById("text")
+let inputEl = document.getElementById("textinput")
 
-bodyEl.addEventListener("change", changeColor);
-inputEl.addEventListener("input", changeText);
+bodyEl.addEventListener("change", changeColor)
+inputEl.addEventListener("input", changeText)
 
 function changeColor(e){ 
-    console.log(colorEl.value);
-    let colorChange = colorEl.value;
+    console.log(colorEl.value)
+    let colorChange = colorEl.value
     
     if (CSS.supports('color', colorChange)){
-        colorEl.style.color = "white";
+        colorEl.style.color = "white"
         bodyEl.style.backgroundColor = colorChange;
     }
     else{
-        colorEl.style.color = "White";
+        colorEl.style.color = "White"
     }
 }
+
+let scrollEl = document.getElementById ("scrollMenu")
 
 function changeText() {
     let textChange = inputEl.value;
@@ -32,3 +34,18 @@ let avsnittEl = document.createElement ("p")
 avsnittEl.innerHTML = "This is your text " + insertText
 divEl.appendChild(avsnittEl)
 divEl.innerHTML += "<h3> This is a text element created with Javascript </h3>"
+
+
+scrollEl.addEventListener("change", changeTextTag);
+
+function changeTextTag() {
+    let selectedTag = scrollEl.value;
+
+    let newEl = document.createElement(selectedTag)
+    newEl.id = "text"
+    newEl.innerText = textEl.innerText;
+
+    textEl.replaceWith(newEl)
+
+    textEl = newEl
+}
